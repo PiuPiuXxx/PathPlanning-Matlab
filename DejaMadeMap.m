cdcc3d27095d4a5766aede48935375473a1c781d
程闱劭 <byr_sunrise@bupt.edu.cn>
2019-11-13 17:59:05 +0800

    寻路算法

---
+++ DejaMadeMap.m
@@ -0,0 +1,33 @@
+function Map = DejaMadeMap(map,start,goal)
+sizemap=size(map);
+%Visualize
+Vertical = sizemap(1);
+Horizontal =sizemap(2);
+%Generate Map as BW Image Every Grid is a 10x10 pixel square bordered by a
+%1 pixel black line
+%Generating one block
+i = [0 ones(1,10) 0];
+Block = [ zeros(1,12); i;i;i;i;i;i;i;i;i;i;zeros(1,12)];
+Map=[];X=[];Y=[];
+for i = 1:Vertical
+    Map = [Map;Block];
+    Y=[Y; 12*(i-1)+6];
+end
+Strip=Map;
+for i = 1:Horizontal-1
+    Map = [Map Strip];
+    X=[X; 12*(i-1)+6];
+end
+[x,y]=find(map);
+for k=1:length(x)
+    [ii i] = min(abs(X-y(k)*12));
+    [jj j] = min(abs(Y-x(k)*12));
+    Map(Y(j)-5:Y(j)+6,X(i)-5:X(i)+6)=0;
+end
+[ii i] = min(abs(X-start(2)*12));
+[jj j] = min(abs(Y-start(1)*12));
+Map(Y(j)-1:Y(j)+2,X(i)-1:X(i)+2)=0;
+[ii i] = min(abs(X-goal(2)*12));
+[jj j] = min(abs(Y-goal(1)*12));
+Map(Y(j)-2:Y(j)+3,X(i)-2:X(i)+3)=0;
+end
---
+++ Run.m
@@ -0,0 +1,22 @@
+% Shirine El Zaatari - Autonomous Mobile Robotics - A* path planning algorithm
+
+%UNCOMMENT THIS SECTION in order to generate your own map
+% Num_vertical_div = 20;
+% Num_horizontal_div = 10;
+%[map,goal,initial,grid]=GenerateMap(20,20);
+
+% OR UNCOMMENT THIS SECTION to randomize a load already made map
+
+clc;clear;
+%读取地图：num为double类型，txt为cell类型，raw为cell类型（同时读取num和txt）
+[num, txt, raw] = xlsread('lab.xlsx');
+start = [1,1];
+goal = [15,71];
+
+grid = DejaMadeMap(num,start,goal);
+
+%A* and outputing result as image
+path = Astar(num,start,goal);
+VisualizePath(path,grid);
+
+
